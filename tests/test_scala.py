e97ff4f31394e73341f410351564491514e5d3f9
Taylor Nation <tnation+github@azavea.com>
2016-08-11 11:59:03 -0400

    Use Ansible Testinfra module instead of Command module and local facts

--- tests/test_scala.py
+++ tests/test_scala.py
@@ -1,29 +1,93 @@
-def test_java_exists(Command):
-	java = Command("java -version")
-	assert java.rc == 0
+import pytest
 
 
-def test_scala_exists(Command, Ansible, SystemInfo):
-	scala_version = Command(
-		"scala -e \"println(util.Properties.versionNumberString)\"")
-	assert scala_version.rc == 0
+@pytest.fixture()
+def AnsibleDefaults(Ansible):
+    """ Load default variables into dictionary.
 
-def test_scala_version(Command, Sudo):
-	with Sudo(user='root'):
-		# Extract target scala version from ansible facts
-		target_scala_version = Command.check_output(
-			"cat /etc/ansible/facts.d/molecule.fact"
-			" | grep target_scala_version | cut -f2 -d'='")
+    Args:
+        Ansible - Requires the ansible connection backend.
+    """
+    return Ansible("include_vars", "./defaults/main.yml")["ansible_facts"]
 
-		# Ensure that the running version of scala is the target version
-		Command.run_test(
-			"scala -e \"println(util.Properties.versionNumberString)\" |"
-			" grep %s" % target_scala_version)
 
-		# Extract target sbt version from ansible facts
-		target_sbt_version = Command.check_output(
-			"cat /etc/ansible/facts.d/molecule.fact |"
-			" grep target_sbt_version | cut -f2 -d'='")
+@pytest.fixture()
+def AnsibleFacts(Ansible):
+    """ Load ansible facts into a dictionary.
 
-		# Ensure that the running version of sbt is the target version
-		Command.run_test("sbt sbtVersion | grep %s" % target_sbt_version)
+    Args:
+        Ansible - Requires ansible connection backend.
+    """
+
+    return Ansible("setup")["ansible_facts"]
+
+
+def test_java_exists(AnsibleFacts, Package):
+    """ Ensure that the proper java version has been installed
+
+    Args:
+        AnsibleFacts - Dictionary of Ansible facts.
+        Package - Module that gives information about package install status
+            and version.
+
+    Raises:
+        AssertionError if the wrong java version is installed.
+    """
+
+    # Ubuntu 16.04 and above only support openjdk 8
+    os_version = AnsibleFacts["ansible_distribution_version"]
+    if float(os_version) >= 16.04:
+        java_major_version = "8"
+    else:
+        java_major_version = "7"
+
+    assert Package("openjdk-{}-jdk".format(java_major_version)).is_installed
+
+
+def test_scala_exists(Command):
+    """ Attempt to run the scala binary to ensure it exists.
+
+    Args:
+        Command - module to run commands in a remote shell.
+
+    Raises:
+        AssertionError if the command's return code is not zero.
+    """
+    Command.run_test(
+        "scala -e \"println(util.Properties.versionNumberString)\"")
+
+
+def test_scala_version(Command, AnsibleDefaults):
+    """ Check that Ansible installed the proper version of scala
+
+    Args:
+         Command - module to run commands in a remote shell.
+         AnsibleDefaults - Dictionary of default ansible variable values.
+
+    Raises:
+        AssertionError if the command's return code is not zero.
+    """
+    # Extract target scala version from ansible facts
+    target_scala_version = AnsibleDefaults["scala_version"]
+
+    # Ensure that the running version of scala is the target version
+    Command.run_test(
+        "scala -e \"println(util.Properties.versionNumberString)\" |"
+        " grep {}".format(target_scala_version))
+
+
+def test_sbt(Package, AnsibleDefaults):
+    """ Check to see if the proper version of SBT is installed.
+
+    Args:
+        Package: module that verifies package install and version
+        AnsibleDefaults - Dictionary of default ansible variable values.
+
+    Raises:
+        AssertionError if the proper version of SBT isn't installed, or SBT
+            is installed at the wrong version.
+    """
+    target_sbt_version = AnsibleDefaults["scala_sbt_version"]
+    installed_version = Package("sbt").version
+    assert Package("sbt").is_installed
+    assert installed_version.startswith(target_sbt_version)
